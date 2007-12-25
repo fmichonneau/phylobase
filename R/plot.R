81a87f95263583d7388cc9af2f2cf0cbdf758390
Thibaut Jombart <t.jombart@imperial.ac.uk>
2007-12-25 11:42:44 +0000

    fixed the spurious warnings in plot for phylo4d

--- R/plot.R
+++ R/plot.R
@@ -6,50 +6,50 @@
 
 
 
-##############
-# plot phylo4
-##############
+###############
+## plot phylo4
+###############
 
 setGeneric("plot")
 setMethod("plot",signature(x="phylo4",y="missing"), function(x,...){
-  if(!require(ape)) stop("the ape package is required")
-  x <- as(x, "phylo")
-  res <- plot(x, ...)
-  return(invisible(res))
+    if(!require(ape)) stop("the ape package is required")
+    x <- as(x, "phylo")
+    res <- plot(x, ...)
+    return(invisible(res))
 }) # end plot phylo4
 
 
 
-###############
-# plot phylo4d
-###############
+################
+## plot phylo4d
+################
 setMethod("plot", signature(x="phylo4d",y="missing"), function(x, type=c("symbols", "squares", "dotchart"), ...){
-  if(!require(ade4)) stop("the ade4 package is required")
-
-  if (any(is.na(tdata(x,which="tip")))) {
-      warning("dropping NA values from tip data for plot")
-      x <- na.omit(x)
-  }
-  dat <- tdata(x, which="tip")
+    if(!require(ade4)) stop("the ade4 package is required")
+    
+    if (any(is.na(tdata(x,which="tip")))) {
+        warning("dropping NA values from tip data for plot")
+        x <- na.omit(x)
+    }
+    dat <- tdata(x, which="tip")
 
      
-  x <- as(x,"phylog")
-  type <- match.arg(type)
-  if(ncol(dat)>1 & type=="symbols") type <- "squares" 
-  if(ncol(dat)==1 | type=="symbols"){
-
-    res <- symbols.phylog(x, squares=dat[,1], ...)
+    x <- suppressWarnings(as(x,"phylog"))
+    type <- match.arg(type)
+    if(ncol(dat)>1 & type=="symbols") type <- "squares" 
+    if(ncol(dat)==1 | type=="symbols"){
+        
+        res <- symbols.phylog(x, squares=dat[,1], ...)
+        
+    } else if(type == "squares"){
+        
+        res <- table.phylog(dat,x,...)
+        
+    } else if(type == "dotchart"){
+        
+        res <- dotchart.phylog(x, dat, ...)
+        
+    }
     
-  } else if(type == "squares"){
-
-    res <- table.phylog(dat,x,...)
-
-  } else if(type == "dotchart"){
-
-    res <- dotchart.phylog(x, dat, ...)
-
-  }
-
-  return(invisible(res))
- 
+    return(invisible(res))
+    
 })
