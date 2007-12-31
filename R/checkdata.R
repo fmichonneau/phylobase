b096d8f4fc27a3fc2beb1612c1875a4c562acc5a
Steven Kembel <steve.kembel@gmail.com>
2007-12-31 21:23:09 +0000

    Extend check_data to check node.data with informative warnings

--- R/checkdata.R
+++ R/checkdata.R
@@ -30,16 +30,14 @@ check_data <- function(object,
 {
 
     ## tip default: use names, require names, must match exactly
-    ##use.tip.names=match.arg(use.tip.names)
     missing.tip.data <- match.arg(missing.tip.data)
     extra.tip.data <- match.arg(extra.tip.data)
     default.tip.names <- match.arg(default.tip.names)
     
     ## node default: don't use node names, don't require names, do not need to match exactly
-    ## use.node.names=match.arg(use.node.names)
     missing.node.data <- match.arg(missing.node.data)
     extra.node.data <- match.arg(extra.node.data)
-    missing.node.names <- match.arg(default.node.names)
+    default.node.names <- match.arg(default.node.names)
     
     ## for each set of data, check for names, missing and extra data and take appropriate actions
     
@@ -62,7 +60,6 @@ check_data <- function(object,
                 }
             }
             
-            ## TODO add check for when length is equal but there is no overlap in names
             ## check tip names
             ## check for missing or extra tip data (relative to tree taxa)
             if (setequal(row.names(object@tip.data), object@tip.label)) {
@@ -147,70 +144,88 @@ check_data <- function(object,
         ## if we want to use node.names
         if (use.node.names) {
             
-            ##check for default names
-            ##could be 1:nTips or nTips+1:nEdges
+            ## check for default names
             if (all(row.names(object@node.data) == 1:length(row.names(object@node.data))) 
-                || all(row.names(object@node.data) == (phylobase::nTips(object)+1):nEdges(object)))
-              {
-                  ##no node.names
-                  if (default.node.names == "fail") {
-                      stop("Node data have default names and may not match node labels. Consider using the use.node.names=FALSE option.")
-                  }
-                  else if (default.node.names == "warn") {
-                      warning("Node data have default names and may not match node labels. Consider using the use.node.names=FALSE option.")
-                  }
-              }
+                || all(row.names(object@node.data) == (phylobase::nTips(object)+1):nEdges(object))) {
+                ## no node.names
+                if (default.node.names == "fail") {
+                    stop("Node data have default names and may not match tree node labels. ",
+                         "Consider using the use.node.names=FALSE option.")
+                }
+                else if (default.node.names == "warn") {
+                    warning("Node data have default names and may not match tree node labels. ",
+                            "Consider using the use.node.names=FALSE option.")
+                }
+            }
             
             ## check node names
             ## check for missing or extra node data (relative to tree taxa)
             if (setequal(row.names(object@node.data), object@node.label)) {
-                ##names are perfect match - ok
+                ## names are perfect match - ok
                 return(TRUE)
             }
-            else
-              {
-                  ##we know the tree taxa and node.data taxa are not a perfect match
-                  
-                  ##if node.data taxa are subset of tree taxa, check missing.node.data arg and act accordingly
-                  if (!all(object@node.label %in% row.names(object@node.data))) {
-                      ##we know it's not an exact match - we have missing.node.data - take action
-                      if (!any(object@node.label %in% row.names(object@node.data))) {
-                          if (missing.node.data == "fail") {
-                              stop("Node data names do not match tree node labels.")
-                          }
-                          else if (missing.node.data == "warn") {
-                              warning("Node data names do not match tree node labels.")
-                          }
-                          
-                      }
-                      else
-                        {
-                            if (missing.node.data == "fail") {
-                                stop("Node data names are a subset of tree node labels.")
-                            }
-                            else if (missing.node.data == "warn") {
-                                warning("Node data are a subset of phylo4 node labels.")
-                            }
+            else {
+                ## we know the tree taxa and node.data taxa are not a perfect match
+                ## if node.data taxa are subset of tree taxa, check missing.node.data arg and act accordingly
+                nodes.in.rownames <- object@node.label %in% row.names(object@node.data)
+                rownames.in.nodes <- row.names(object@node.data) %in% object@node.label 
+                missing.data.names <- object@node.label[!nodes.in.rownames]
+                missing.data.name.msg <- if (length(missing.data.names)==0) "" else {
+                    paste("\n(missing data names: ",
+                          paste(missing.data.names,collapse=","),")",sep="")
+                }
+                extra.data.names <- row.names(object@node.data)[!rownames.in.nodes]
+                extra.data.name.msg <- if (length(extra.data.names)==0) "" else {
+                    paste("\n(extra data names: ",
+                          paste(extra.data.names,collapse=","),")",sep="")
+                }
+                if (!all(nodes.in.rownames)) {
+                    ## we know it's not an exact match - we have missing.node.data - take action
+                    if (!any(nodes.in.rownames)) {
+                        errmsg <- paste("No matches between node data names and tree node labels.",
+                                        missing.data.name.msg,extra.data.name.msg)
+                        if (missing.node.data == "fail") {
+                            stop(errmsg)
                         }
-                      ##else ok
-                  }
-                  
-                  ##if tree taxa are subset of node.data, check extra.node arg and act accordingly
-                  if (!all(row.names(object@node.data) %in% object@node.label)) {
-                      ##we know it's not an exact match - we have missing.node.data - take action
-                      ##fail
-                      if (extra.node.data == "fail") {
-                          stop("Node data names are a superset of tree node labels.")
+                        else if (missing.node.data == "warn") {
+                            warning(errmsg)
+                        }
+                    }
+                    else
+                      {
+                          errmsg <- paste("Node data names are a subset of tree node labels",
+                                          missing.data.name.msg,
+                                          extra.data.name.msg)
+                          if (missing.node.data == "fail") {
+                              stop(errmsg)
+                          }
+
+                          else if (missing.node.data == "warn") {
+                              warning(errmsg)
+                          }
                       }
-                      ##warn
-                      else if (extra.node.data == "warn") {
-                          warning("Node data names are a superset of tree node labels.")
-                      }
-                      ##else ok
-                  }
-                  
-                  return(TRUE)
-              } 
+                    ##else ok
+                }
+                
+                ##if tree taxa are subset of node.data, check extra.node arg and act accordingly
+                if (!all(row.names(object@node.data) %in% object@node.label)) {
+                    ##we know it's not an exact match - we have extra.node.data - take action
+                    ##fail
+                    errmsg <- paste("Node data names are a superset of tree node labels",
+                                    missing.data.name.msg,
+                                    extra.data.name.msg)
+                    if (extra.node.data == "fail") {
+                        stop(errmsg)
+                    }
+                    ##warn
+                    else if (extra.node.data == "warn") {
+                        warning(errmsg)
+                    }
+                    ##else ok
+                }
+                
+                return(TRUE)
+            } 
         }
         else
           {
@@ -220,7 +235,6 @@ check_data <- function(object,
               }
           }
     }
-
 }
 
 attach_data <- function(object,
@@ -230,13 +244,7 @@ attach_data <- function(object,
 {
     
     ## assumes data have already been checked by check_data!
-    
-    ## clean empty names - if data are imported as 'all' but only tips or nodes have names, clean up
-                                        #if (all(row.names(object@tip.data)==""))
-                                        #	row.names(object@tip.data) <- NULL
-                                        #if (all(row.names(object@node.data)==""))
-                                        #	row.names(object@node.data) <- NULL
-    
+
     ## for each set of data, take appropriate actions
     
     ## tip data operations:
