ed3cb36b0b9499748f4719d9f9193d80f8d17605
Peter Cowan <rforge@pcowan.com>
2008-01-29 02:58:17 +0000

    attempt to roll back to pre ioNCL commit

--- R/RcppVersion.R
+++
@@ -1,6 +0,0 @@
-RcppVersion <- function() {
-
-  # Print version of Rcpp/RcppTemplate used to build this package
-  licenseFile <- file(system.file(".","LICENSE-Rcpp.txt",package="ReadWithNCL"),"r")
-  writeLines(readLines(licenseFile)[1:4])
-}
