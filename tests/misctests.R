61b574039fb7f5a8f5e28326bfec12eedf77c3c1
Ben Bolker <bbolker@gmail.com>
2009-08-18 18:33:08 +0000

    added set.seed(1)

--- tests/misctests.R
+++ tests/misctests.R
@@ -35,6 +35,7 @@ p1C <- na.omit(p1)
 labels(p1C) <- tolower(labels(p1C))
 
 ## trace("prune",browser,signature="phylo4d")
+set.seed(1)
 r1 <- rcoal(5)
 
 ## trace("phylo4d", browser, signature = "phylo")
