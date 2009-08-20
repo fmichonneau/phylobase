c73fd62f2dc6c19442bb2795488265a1729715cd
Jim Regetz <regetz@nceas.ucsb.edu>
2009-08-20 07:49:58 +0000

    added a NAMESPACE file, and replaced .First.lib function with .onLoad (but left the former there for now, commented out)

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,11 @@
-.First.lib <- function(lib, pkg) {
-	require(ape)
-       library.dynam("phylobase", pkg, lib )
+# only used when there is no namespace
+#.First.lib <- function(lib, pkg) {
+#	require(ape)
+#       library.dynam("phylobase", pkg, lib )
+#}
+
+# use this with a namespace
+.onLoad <- function(lib, pkg) {
+    require(ape)
+    require(methods)
 }
-
-
