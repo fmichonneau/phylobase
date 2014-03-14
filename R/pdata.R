452160bc462fc76b5585e579d805ae266246915a
Francois Michonneau <francois.michonneau@gmail.com>
2014-03-14 20:36:31 +0000

    moving chunk of code that was in non-standard folder

--- R/pdata.R
+++ R/pdata.R
@@ -2,6 +2,20 @@
 ptypes <- c("multitype","binary","continuous","DNA","RNA","aacid",
             "other","unknown")
 
+#' Class "pdata"
+#' 
+#' Data class for phylo4d objects
+#' 
+#' 
+#' @name pdata-class
+#' @aliases ptypes pdata-class [<-,pdata-method [,pdata-method
+#' [,pdata,ANY,ANY,ANY-method [[,pdata-method [[<-,pdata-method
+#' [[,pdata,ANY,ANY-method [[,pdata,ANY,missing-method
+#' @docType class
+#' @section Objects from the Class: Objects can be created by calls of the form
+#' \code{new("pdata", ...)}.
+#' @author Ben Bolker
+#' @keywords classes
 setClass("pdata", representation(data="data.frame",
                                  type="factor",
                                  comment="character",
@@ -10,6 +24,25 @@ setClass("pdata", representation(data="data.frame",
            comment=character(0),metadata=list()))
 
 ## pdata constructor
+
+
+#' Constructor for pdata (phylogenetic data) class
+#' 
+#' Combine data, type, comments, and metadata information to create a new pdata
+#' object, or check such an object for consistency
+#' 
+#' 
+#' @aliases pdata check_pdata
+#' @param data a data frame
+#' @param type a factor with levels as specified by \linkS4class{pdata}, the
+#' same length as \code{ncol(data)}
+#' @param comment a character vector, the same length as \code{ncol(data)}
+#' @param metadata an arbitrary list
+#' @param object an object of class \code{pdata}
+#' @return An object of class \code{pdata}
+#' @author Ben Bolker
+#' @seealso \linkS4class{pdata}
+#' @keywords misc
 pdata <- function(data,type,comment,metadata) {
   nvar <- ncol(data)
   if (missing(type)) {
@@ -73,3 +106,38 @@ setMethod("plot",signature(x="pdata",y="missing"), function(x,...){
 ## z[[2]]
 
 ## test conflict resolution error
+
+#######
+### old code retrieved from misc/ folder
+
+## setClass("pdata", representation(x="vector", y="vector"))
+## setMethod("[","pdata",function(x,i, j,...,drop=TRUE)new("pdata",x=x@x[i],y=x@y[i]))
+
+# x <- new("pdata", x=c("a","b", "c", "d", "3"), y=c(1:5))
+#>x[c(2,4)]
+#An object of class “pdata”
+#Slot "x":
+#[1] "b" "d"
+#
+#Slot "y":
+#[1] 2 4
+
+
+
+# doesn't work
+#setClass("track", representation("list", comment="character", metadata="vector"), contains="list", prototype(list(), comment="", metadata=NA))
+#setMethod("[","track",function(x,i, j,...,drop=TRUE)new("track", list(lapply(x, function(x, i, j, ..., drop=TRUE) x@.Data[i]))))
+
+# this works, how to incorporate into method above?
+#> lapply(x, function(x, i=2, j, ..., drop=TRUE) x@.Data[i])
+#$x
+#[1] "b"
+
+#$y
+#[1] 2
+
+# this works, but list structure is destroyed
+#> mapply(function(x, i, j, ..., drop=TRUE) x@.Data[i], x, 2)
+#  x   y 
+#"b" "2" 
+
