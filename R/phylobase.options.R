3f484b85d951575cfa302739214f11fd988f1337
Francois Michonneau <francois.michonneau@gmail.com>
2014-04-08 15:20:40 +0000

    updated roxygen formatting for phylobase.options

--- R/phylobase.options.R
+++ R/phylobase.options.R
@@ -1,29 +1,27 @@
-### Modified code from package sm
-
-
-#' Set or return options of phylobase
-#' 
-#' Provides a mean to control the validity of \code{phylobase} objects such as
-#' singletons, reticulated trees, polytomies, etc.
-#' 
-#' The parameter values set via a call to this function will remain in effect
-#' for the rest of the session, affecting the subsequent behavior of phylobase.
-#' 
-#' @param \dots a list may be given as the only argument, or any number of
-#' arguments may be in the \code{name=value} form, or no argument at all may be
-#' given.  See the Value and Details sections for explanation.
-#' @return A list with the updated values of the parameters. If arguments are
-#' provided, the returned list is invisible.
-#' @author Francois Michonneau (adapted from the package \code{sm})
-#' @keywords phylobase validator
-#' @examples
-#' 
-#' \dontrun{
-#' phylobase.options(poly="fail")
-#' # subsequent trees with polytomies will fail the validity check
-#' }
-#' 
-#' @export phylobase.options
+##' Set or return options of phylobase
+##' 
+##' Provides a mean to control the validity of \code{phylobase}
+##' objects such as singletons, reticulated trees, polytomies, etc.
+##' 
+##' The parameter values set via a call to this function will remain
+##' in effect for the rest of the session, affecting the subsequent
+##' behavior of phylobase.
+##' 
+##' @param \dots a list may be given as the only argument, or any
+##' number of arguments may be in the \code{name=value} form, or no
+##' argument at all may be given.  See the Value and Details sections
+##' for explanation.
+##' @return A list with the updated values of the parameters. If
+##' arguments are provided, the returned list is invisible.
+##' @author Francois Michonneau (adapted from the package \code{sm})
+##' @keywords phylobase validator
+##' @examples
+##' \dontrun{
+##' phylobase.options(poly="fail")
+##' # subsequent trees with polytomies will fail the validity check
+##' }
+##' 
+##' @export phylobase.options
 phylobase.options <- function (...) {
     if (nargs() == 0) return(.phylobase.Options)
     current <- .phylobase.Options
