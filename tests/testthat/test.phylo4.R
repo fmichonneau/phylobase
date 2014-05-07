70f82f855cf5f9b5ec87ca4e99a1accdb9e8d4d3
Francois Michonneau <francois.michonneau@gmail.com>
2014-05-07 22:14:15 +0000

    move/update phylo4 tests

--- tests/testthat/test.phylo4.R
+++ tests/testthat/test.phylo4.R
@@ -5,7 +5,7 @@
 # phylo4.R is mostly used to set generics, so no testing needed
 
 # one non-exported method:
-test..genlab <- function() {
-    # use phylobase:::.genlab
-}
+## test..genlab <- function() {
+##     # use phylobase:::.genlab
+## }
 
