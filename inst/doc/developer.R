55602ee61594901342754c9020a06a6a239979bf
Francois Michonneau <francois.michonneau@gmail.com>
2014-11-29 07:43:44 -0500

    removed inst/doc from gitignore to avoid warnings

---
+++ inst/doc/developer.R
@@ -0,0 +1,13 @@
+## ----include=FALSE-------------------------------------------------------
+library(knitr)
+opts_chunk$set(
+    fig.keep='none', dev='pdf', fig.width=6, fig.height=6,
+    latex.options.color="usenames,dvipsnames"
+)
+
+## ----setOptions, echo=FALSE-----------
+options(width=40)
+
+## ----RforgeInstall, eval=FALSE--------
+#  install.packages("phylobase", repos = "http://r-forge.r-project.org")
+
