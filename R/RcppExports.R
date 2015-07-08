760c05e098f39ec7506e74e4a0a3915ceee14028
Francois Michonneau <francois.michonneau@gmail.com>
2015-07-07 20:41:25 -0400

    convert to using rncl instead of NCL

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -77,7 +77,3 @@ checkTreeCpp <- function(obj, opts) {
     .Call('phylobase_checkTreeCpp', PACKAGE = 'phylobase', obj, opts)
 }
 
-GetNCL <- function(params, paramsVecR) {
-    .Call('phylobase_GetNCL', PACKAGE = 'phylobase', params, paramsVecR)
-}
-
