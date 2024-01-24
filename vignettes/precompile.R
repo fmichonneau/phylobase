7e8299c58c12eb982b2fe2f116e31b6536568a1f
François Michonneau <francois.michonneau@gmail.com>
2024-01-24 11:59:17 +0100

    precompile vignette

---
+++ vignettes/precompile.R
@@ -0,0 +1,18 @@
+make_vignettes <- function() {
+  vignettes <- list.files("vignettes", pattern = ".Rmd.orig", full.names = TRUE)
+  purrr::walk2(
+    vignettes,
+    file.path("vignettes", gsub("\\.orig", "", basename(vignettes))),
+    function(x, y) {
+      knitr::knit(x, output = y)
+    }
+  )
+  ## move the figures
+  figures <- list.files(".", pattern = "fig-vignettes-.+\\.png$", full.names = TRUE)
+  res_mv <- file.rename(figures, file.path("vignettes", figures))
+  stopifnot(all(res_mv))
+  TRUE
+}
+make_vignettes()
+library(devtools)
+devtools::build_vignettes()
